a6782c3d6941fdc15d87348de1d5edabdebfd478
jonyoung123 <stephenjohn5646@gmail.com>
2022-10-27 23:15:46 -0700

    chore update server setup

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const path = require("path");
+const tokenValidator = require('./services/token-service');
+
+// ROUTERS
+const itemRouter = require('./routes/items.routes')
+const userRouter = require('./routes/user.routes');
+
+
+// CONSTANTS
+const apiPaths = require('./constants/api-paths')
+
+// APPLICATION CONFIGURATIONS
+const app = express();
+const hostname = "127.0.0.1";
+const port = process.env.PORT || 8080;
+
+// IMPORTANT MIDDLEWARES
+app.use(express.json())
+
+// SECURITY MIDDLEWARE
+app.use('/', (req, res, next) => {
+    
+    try{
+        
+        if( req.url.includes('signup')){
+            console.log('yes')
+            return next();
+        }
+        const token = req.headers['authorization'].replace('Bearer ', '').trim();
+        const tokenValid = tokenValidator(token);
+        if(!token){
+            return res.status(401).json({message :'Unauthorized.'});
+        }
+        next();
+    }catch(err){
+        console.log(err)
+        return res.status(401).json({message :'Forbidden'})
+    }
+
+})
+
+// APPLICATION ROUTERS
+app.use(apiPaths.allItems, itemRouter)
+app.use(apiPaths.userPath, userRouter)
+
+// APPLICATION STARTING
+app.listen(port, ()=>{
+    console.log(`server listening at port ${port}`);
+});
